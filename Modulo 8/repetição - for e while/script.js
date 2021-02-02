e909fba9d0027d243230cde804223d4b3bb3bda5
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2021-02-02 18:15:08 -0300

    Add files via upload

---
+++ Modulo 8/repetição - for e while/script.js
@@ -0,0 +1,18 @@
+// var numero = 5;
+
+// for(var vez = 0; vez < numero; vez++){
+//     console.log("Executando o for", vez)
+// }
+
+
+
+var numero = Math.random() * 100;
+
+while (numero < 90) {
+    console.log(numero);
+    numero++;
+}
+
+console.log(numero);
+
+console.log("Acabou!");
