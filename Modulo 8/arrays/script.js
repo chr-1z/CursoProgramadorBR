50fbf1c04d10bd3f5f88ce9fb47854b48479150b
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2021-02-02 18:13:22 -0300

    Add files via upload

---
+++ Modulo 8/arrays/script.js
@@ -0,0 +1,13 @@
+var alunos = ["Igor", "José", "Marcos", "Mariana", "Joana"];
+
+// for (var i = 0; i < alunos.length; i++){
+//     console.log(alunos[i]);
+// }
+
+// for (var i in alunos) {
+//     console.log(alunos[i]);
+// }
+
+for (var aluno of alunos) {
+    console.log(aluno);
+}
