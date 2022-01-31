3d5461d41261b546d07eb31271be996cb07e98ce
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2022-01-31 17:33:23 -0300

    update: jsx

--- Modulo 31 - ReactJS/index.js
+++
@@ -1,8 +0,0 @@
-const root = document.querySelector("#root");
-
-t1 = React.createElement("h1", {id:"Título principal"}, "Hello World!");
-t2 = React.createElement("h4", {id:"Título complementar"}, "Hello World!");
-
-const headline = React.createElement("div", null, t1, t2);
-
-ReactDOM.render(headline, root);
