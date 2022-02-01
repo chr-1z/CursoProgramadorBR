e0cb0361b2ae72b6a6c4e52cf52f1bd466ec2793
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2022-02-01 11:20:40 -0300

    update: lists

---
+++ Modulo 31 - ReactJS/meu-app/meu-app-2/src/App.js
@@ -0,0 +1,10 @@
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <Header name="chr1z's page" links={["Sobre", "Comprar", "Contato"]}></Header>
+  );
+}
+
+export default App;
