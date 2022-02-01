12ba78895147ba6137865065b623346cd0c50c10
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2022-02-01 10:48:04 -0300

    update: updates

--- Modulo 31 - ReactJS/meu-app/not build/src/Header.js
+++ Modulo 31 - ReactJS/meu-app/not build/src/Header.js
@@ -7,9 +7,21 @@ function Header(props) {
         <header className="header">
             <h1 className="logo">{props.name}</h1>
             <ul className="menu">
-                <li className="about">{props.links[0]}</li>
-                <li className="buy">{props.links[1]}</li>
-                <li className="contact">{props.links[2]}</li>
+                <li className="about" key={props}>
+                    <a href="https://chr-1z.github.io/my-portfolio/" target="_blank">
+                        {props.links[0]}
+                    </a>
+                </li>
+                <li className="buy" key={props}>
+                    <a href="https://pt.aliexpress.com/item/1005001877479051.html?gatewayAdapt=glo2bra&aff_fcid=c158bfea5a7b4d5c9e4ee93b5609a69c-1642794863003-06694-_9AkBBN&tt=CPS_NORMAL&aff_fsk=_9AkBBN&aff_platform=shareComponent-detail&sk=_9AkBBN&aff_trace_key=c158bfea5a7b4d5c9e4ee93b5609a69c-1642794863003-06694-_9AkBBN&terminal_id=e8321b117a66425ea87706d7a798f084&afSmartRedirect=n" target="_blank">
+                        {props.links[1]}
+                    </a>
+                </li>
+                <li className="contact" key={props}>
+                    <a href="mailto:chris99leichtweiss@gmail.com"> 
+                        {props.links[2]}
+                    </a>
+                </li>
             </ul>
         </header>
     );
