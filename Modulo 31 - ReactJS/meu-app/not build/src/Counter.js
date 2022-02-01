d2c9a88f65c693865dd370b3a4de059b6f778b7d
Christian Oliveira Leichtweis <chris99leichtweiss@gmail.com>
2022-02-01 18:04:02 -0300

    update: life cicle methods

--- Modulo 31 - ReactJS/meu-app/not build/src/Counter.js
+++ Modulo 31 - ReactJS/meu-app/not build/src/Counter.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 class Counter extends React.Component {
 
+    // 1° método do ciclo de vida - construtor
     constructor(props) {
         super(props);
 
@@ -11,14 +12,34 @@ class Counter extends React.Component {
     }
 
     add() {
-        //this.setState( { count: this.state.count + 1 } ); //maneira errada, pois pode bugar o valor de state e não mostrar o valor real na tela
-        this.setState((state) => {return { count: this.state.count + 1 }});
+        // this.setState( { count: this.state.count + 1 } ); //maneira errada, pois pode bugar o valor de state e não mostrar o valor real na tela
+        // this.setState((state) => {return { count: this.state.count + 1 }});
+        
+        // ver se altera no console via callback
+        this.setState((state) => { return { count: state.count + 1 }}, () => {
+            console.log(this.state);
+            localStorage.setItem("state", JSON.stringify(this.state));
+        });
 
     }
 
     remove() {
-        //this.setState( { count: this.state.count - 1 } ); //maneira errada, pois pode bugar o valor de state e não mostrar o valor real na tela
+        // this.setState( { count: this.state.count - 1 } ); //maneira errada, pois pode bugar o valor de state e não mostrar o valor real na tela
         this.setState((state) => {return { count: this.state.count - 1 }});
+
+    }
+
+    shouldComponentUpdate() {
+        return true;
+    }
+
+    // é executado depois que o componente for mostrado na tela 
+    componentDidMount() {
+        
+    }
+
+    // por exemplo um todo list - quando um componente vai sair da tela
+    componentWillUnmount() {
 
     }
 
